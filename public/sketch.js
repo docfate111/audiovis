9acccde32780b3bfca9ecb0cf7b8f1c3d76247a1
docfate111 <tdwilliamsiv@gmail.com>
2020-11-03 20:04:27 -0600

    made circle of sound

--- public/sketch.js
+++ public/sketch.js
@@ -1,19 +1,42 @@
-let mic;
+let mic, amp, song;
+let volHistory = [];
+function preload() {  
+  song = loadSound('sounds/arise.mp3');
+}
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   // background(100);
-  mic = new p5.AudioIn();
-  mic.start();
-  text('Click to start', 20, 20);
-  background(255);
+  song.play();
+  //mic = new p5.AudioIn();
+  amp = new p5.Amplitude();
+  //mic.start();
+  angleMode(DEGREES);
+  background(0);
+  textSize(32);
+  fill(255, 255, 255);
+  text('Click to activate the page', 3*window.innerWidth/8, 35);
 }
-
 function touchStarted() {
   getAudioContext().resume();
 }
-
 function draw() {
-  let vol = mic.getLevel();
-  ellipse(height/2, width/2, vol*5000, vol*5000);
-  console.log('volume', 5000*vol);
+  let vol = amp.getLevel();
+  volHistory.push(vol);
+  translate(width/2, height/2)
+  noFill();
+  beginShape();
+  for (let i = 0; i < 360; i++) {
+    stroke(255);
+    console.log(volHistory[i]);
+    let r = map(volHistory[i], 0, 1, 10, 300);
+    let x = r * cos(i);
+    let y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape();
+
+  if(volHistory.length > 360) {
+    volHistory = [];
+    console.log('reset');
+  }
 }
